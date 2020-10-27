3b01b57b15093c6a3ec9cda4ff794d8cfbd03084
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2020-10-26 20:10:42 -0700

    updated seafood recipe

---
+++ src/shared/seafood.js
@@ -0,0 +1,26 @@
+export const Seafood = [
+    {
+        id: 0,
+        name: "Grilled Salmon",
+        image: "../images/grilled_salmon.jpg",
+        ingredients: "Skin-on salmon fillets, Olive oil, black pepper, kosher salt, Juice from a lemon, chopped parsley, chopped shallot, Dijon mustard",
+        instructions: "https://www.southernliving.com/recipes/easy-grilled-salmon",
+        servings: "4"
+    },
+    {
+        id: 1,
+        name: "Rum Glazed Shrimp",
+        image: "../images/rum-glazed-shrimp.jpg",
+        ingredients: "Peeled and Deveined Shrimp, Olive oil, Sweet Chili sauce, soy sauce, Captain Morgan Spiced Rum, minced garlic, lime juice, crushed red pepper flakes, green onion",
+        instructions: "https://www.delish.com/cooking/a26551522/spiced-rum-glazed-shrimp-recipe/",
+        servings: "4"
+    },
+    {
+        id: 2,
+        name: "Garlic Parmesan Flounder",
+        image: "../images/flounder.png",
+        ingredients: "Flounder fillets, Olive oil, ground black pepper, grated parmesan cheese, bread crumbs, minced garlic, lemon juice and zest",
+        instructions: "https://www.delish.com/cooking/recipe-ideas/a26595710/garlic-parmesan-baked-flounder-recipe/",
+        servings: "4"
+    }
+];
